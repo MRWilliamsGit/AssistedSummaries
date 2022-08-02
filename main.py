d2be33ca7b69e1e69e015a898252ed1336ca8a57
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-08-02 11:54:44 -0400

    simple summarization

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from scripts.APICaller import search_twitter
+from scripts.helpers import API_call, Data_prep
+from scripts.summ_class import GenFinSummarizer
+import json
 
 def main():
     term = "abortion"
+    gfs = GenFinSummarizer()
 
-    work = search_twitter(term)
-    #work = connect_to_endpoint()
-    print(work)
+    #r = API_call(term, 10)
+    #print(r)
 
-    #tweets = get_tweets(term)
-    #tweets = convert_to_df(tweets)
+    #with open('data2.json', 'w') as f:
+    #    json.dump(r, f)
+
+    with open('data2.json', 'r') as myfile:
+        r = json.load(myfile)
+
+    work, workdf = Data_prep(r)
+
+    text_list = gfs.make_cloud_chunks(workdf)
+    output = gfs.summarize(text_list, length=400)
+
+    print(output)
+
 
 # Execute main function
 if __name__ == "__main__":
