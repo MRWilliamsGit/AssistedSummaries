140836f2712bc9161c0118feb933efdb83e0f373
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-08-02 16:55:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,44 +18,43 @@ def main():
                                 min_value=2, 
                                 max_value=5,
                                 value=2)
-    #clusters = 2
 
-    #execute once input is collected
-    if term != " " and clusters != " ":
-
+    if term != " ":
+        #call API
+        with st.spinner("Collectiong Data"):
             #first collect info from API
-            # r = API_call(term, 100)
-                   # print(r)
-
+            r = API_call(term, 50)
             # with open('data3.json', 'w') as f:
             #    json.dump(r, f)
 
-            with open("data\data2.json", "r", encoding="utf8") as myfile:
-                r = json.load(myfile)
+            #with open("data\data3.json", "r", encoding="utf8") as myfile:
+            #    r = json.load(myfile)
 
-            # if info is collected, summarize in clusters
+            # if info is not collected, stop there
             if r == "Oops":
                 st.error("Twitter data could not be loaded at this time")
-            else:
-                #initialize models
-                with st.spinner("Generating Summaries"):
-                    gfs = GenFinSummarizer()
-                    cc = ClusterClass(clusters)
+                st.stop()
 
-                    #prep data and generate embeddings
-                    work, workdf = gfs.Data_prep(r)
-                    emb = cc.vectorize_text(work)
+        #cluster text
+        with st.spinner("Clustering Tweets"):
+            #initialize models
+            gfs = GenFinSummarizer()
+            cc = ClusterClass(clusters)
 
-                    #cluster text
-                    clusterdf = cc.k_cluster_text(work, emb)
+            #prep data and generate embeddings
+            work, workdf = gfs.Data_prep(r)
+            emb = cc.vectorize_text(work)
 
-                    #generate summaries for clusters
-                    for i in range(clusters):
-                        text_list = gfs.make_cloud_chunks(clusterdf[clusterdf['cluster']==i])
-                        output = gfs.summarize(text_list, length=200)
-                        #print(output)
-                        st.write(output)
+            #cluster text
+            clusterdf = cc.k_cluster_text(work, emb)
 
+        #generate summaries for clusters
+        with st.spinner("Generating Summaries"):
+            for i in range(clusters):
+                text_list = gfs.make_cloud_chunks(clusterdf[clusterdf['cluster']==i])
+                output = gfs.summarize(text_list, length=200)
+                st.write("Perspective #"+str(i+1)+":")
+                st.write(output)
 
 # Execute main function
 if __name__ == "__main__":
